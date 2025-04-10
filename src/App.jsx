79e60580b80579bab74bad502b414fc63d3399d1
ale <alexlab84@hotmail.com>
2025-04-10 11:58:19 +0200

    Initialize project with Vite, React and MUI

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button, Container, Typography } from '@mui/material'
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Lugares por Descubrir
+      </Typography>
+      <Button variant="contained" color="primary">
+        ¡Hola mundo con MUI!
+      </Button>
+    </Container>
+  )
+}
+
+export default App
